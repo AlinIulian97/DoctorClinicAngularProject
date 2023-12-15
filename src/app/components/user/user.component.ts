5293fae20171034d0796b777665b9d97b8e32905
AlinIulian97 <alin_iulia_20072yahoo.com>
2023-12-15 15:15:33 +0200

    User_Create

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { User } from 'src/app/common/user';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit
+ {
+
   users: User[] = [];
-  
+  findUser: User;
+  userId: number;
+
+  constructor(private userService: UserService) {}
+
+
+  ngOnInit(): void {
+  this.getUser;
+ 
+   }
+
+
+
+  getUser(userId) : void {
+   this.userService.getUser(userId).subscribe(
+    data => this.findUser = data
+   );
+   console.log(this.findUser.password);
+   console.log(this.findUser.userName);
+  }
+
+
+
 }
+
+  
+
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
 import { User } from '../common/user';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private userUrl = `http://localhost:8081/api/users`;
+
+
+  private baseUrl = `http://localhost:8081/api/users`;
   
   constructor(private httpClient: HttpClient ) { }
   
+
+  getUser(userId:number): Observable<User> {
+     const urlUser =(`${this.baseUrl}/${userId}`)
+     return this.httpClient.get<User>(urlUser);
+  };
 }
+
+
 
 
 
@@ -20,6 +30,6 @@ export class UserService {
 interface GetResponseDoctors{
   _embedded :{
     user:User[];
-
+   
   };
 }
